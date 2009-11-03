bc1ae0755061c6f1403c83746fffd0ff1e42dc39
Charles Choukalos <choukalos@gmail.com>
2009-11-03 13:27:19 -0600

    fix routes v1

--- generators/rails_reporting_plugin/rails_reporting_plugin_generator.rb
+++ generators/rails_reporting_plugin/rails_reporting_plugin_generator.rb
@@ -1,9 +1,11 @@
+require File.expand_path(File.dirname(__FILE__) + "/lib/insert_routes.rb")
+
 class RailsReportingPluginGenerator < Rails::Generator::Base 
   
   def manifest
     record do |r|
       r.migration_template 'migration.rb', "db/migrate"
-      r.rotue_resource :p_reports
+      r.route_resource :p_reports
     end
   end
   
