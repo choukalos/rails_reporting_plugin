15470662c490f38de0fe8320451f924224acf205
Charles Choukalos <choukalos@gmail.com>
2009-11-03 11:39:47 -0600

    updated install.rb

--- install.rb
+++ install.rb
@@ -4,8 +4,8 @@ dir = File.dirname(__FILE__)
 # Install Controllers
 FileUtils.cp File.join(dir, 'lib', 'controllers', 'p_reports_controller.rb'), File.join(dir, '../../../app/controllers/p_reports_controller.rb')
 # Install Models
-FileUtils.cp File.join(dir, 'lib', 'models', 'p_reports.rb'), File.join(dir, '../../../app/models/p_reports.rb')
+FileUtils.cp File.join(dir, 'lib', 'models', 'p_report.rb'), File.join(dir, '../../../app/models/p_report.rb')
 # Install Views
 FileUtils.cp_r Dir[File.join(dir, 'lib', 'views', '*')], File.join(dir, '../../../app/views')
 # Show the Install documentation
-puts IO.read(File.join(File.dirname(__FILE__), 'INSTALL'))
+puts IO.read(File.join(File.dirname(__FILE__), 'README'))
